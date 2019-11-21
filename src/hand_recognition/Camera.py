2d276700f807060f42992411249f494371e6104e
randor12 <rynicholas@vt.edu>
2019-11-21 12:06:31 -0500

    Update Camera.py

--- src/hand_recognition/Camera.py
+++ src/hand_recognition/Camera.py
@@ -4,6 +4,7 @@
 :description: This code communicates with the camera
 """
 
+import cv2 as cv
 
 class Camera:
     """
@@ -14,5 +15,20 @@ class Camera:
         """
         Constructor
         """
-        self.x = 0
-        self.y = 0
+        self.cap = cv.VideoCapture(0)  # start capturing video
+        if not self.cap.isOpened():
+            print("Can not open camera")
+            exit(0)
+        while True:
+            # Capture frame-by-frame
+            ret, frame = self.cap.read()
+
+            # if frame is read correctly, ret is True
+            if not ret:
+                print("Cannot receive frame (stream end?). Exiting...")
+                break
+            if frame is None:
+                print("No frame")
+                break
+
+            # Operations on the frame here
