1d51d0864f3005e9e341e185cf009001470fafff
randor12 <rynicholas@vt.edu>
2019-11-21 11:54:49 -0500

    Initialized python code

---
+++ src/hand_recognition/__init__.py
@@ -0,0 +1,9 @@
+"""
+:author: Ryan Nicholas
+:date: November 21, 2019
+:description: The purpose of this repository is to recognize hands using a camera
+"""
+
+
+if __name__ == '__main__':
+    print("Starting code")
